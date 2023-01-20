7b46a5a1a41af008e4cf2c445f8288964dd17dbb
Ashlyn Knox <ashlyn@lilyx.io>
2023-01-19 17:01:16 -0700

    temporarily removed unused code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,15 @@
-import React from "react";
-import clsx from "clsx";
-import Link from "@docusaurus/Link";
-import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
-import Layout from "@theme/Layout";
+import React from 'react';
+// import clsx from 'clsx';
+// import Link from '@docusaurus/Link';
+// import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Layout from '@theme/Layout';
 
-import styles from "./index.module.css";
-
+// import styles from './index.module.css';
+/*
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
   return (
-    <header className={clsx("hero hero--primary", styles.heroBanner)}>
+    <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
@@ -18,13 +18,13 @@ function HomepageHeader() {
     </header>
   );
 }
-
+*/
 export default function Home(): JSX.Element {
-  const { siteConfig } = useDocusaurusContext();
+  // const { siteConfig } = useDocusaurusContext();
   return (
     <Layout>
       <main>
-        <p>Main on index page</p>
+        <p className="text-5xl">Main on index page</p>
       </main>
     </Layout>
   );
