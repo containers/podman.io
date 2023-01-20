bf9b9e1580bac75899bd7a27282c4edb3da2fad2
Ashlyn Knox <ashlyn@lilyx.io>
2023-01-20 13:38:57 -0700

    font configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  corePlugins: {},
-  content: ['./src/**/*.{js,jsx,ts,tsx}', '../docs/**/*.mdx'], // my markdown stuff is in ../docs, not /src
-  darkMode: ['class', '[data-theme="dark"]'], // hooks into docusaurus' dark mode settigns
-  theme: { colors: {}, fontFamily: {}, extend: { backgroundImage: {} } },
+  corePlugins: { preflight: false },
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  darkMode: ["class", '[data-theme="dark"]'],
+  theme: {
+    fontFamily: {
+      display: ["Montserrat", "ui-sans-serif", "system-ui", "sans-serif"],
+      text: ['"Source Sans Pro"', "ui-sans-serif", "system-ui", "sans-serif"],
+      mono: ['"Source Code Pro"', "ui-monospace", "monospace"],
+    },
+    extend: { backgroundImage: {}, colors: {} },
+  },
   plugins: [],
 };
