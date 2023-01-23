d7a9a47e3e6569513eaa973f66a497efb909b02e
Ashlyn Knox <ashlyn@lilyx.io>
2023-01-23 11:29:01 -0700

    tailwind color set

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,17 +4,38 @@ module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   darkMode: ['class', '[data-theme="dark"]'],
   theme: {
+    colors: {
+      black: '#000000',
+      blue: {
+        100: '#D698FF',
+        300: '#83F1FF',
+        500: '#1ECCFF',
+        700: '#009FFF',
+        900: '#0D569B',
+      },
+      gray: {
+        100: '#514c5c',
+        300: '#A5A0B1',
+        500: '#7A7485',
+        700: '#A5A0B1',
+        900: '#2B2735',
+      },
+      purple: {
+        100: '#E1C5FF',
+        300: '#B39ADD',
+        500: '#8870B0',
+        700: '#5F4986',
+        900: '#37255D',
+      },
+      white: '#FFFFFFF',
+    },
     fontFamily: {
       display: ['Montserrat', 'ui-sans-serif', 'system-ui', 'sans-serif'],
       text: ['"Source Sans Pro"', 'ui-sans-serif', 'system-ui', 'sans-serif'],
       mono: ['"Source Code Pro"', 'ui-monospace', 'monospace'],
     },
-    extend: {
-      backgroundImage: {
-        'hero-water': 'url(/images/original/water-background-1370w-605h.png',
-      },
-      colors: {},
+    backgroundImage: {
+      'waterTexture': 'url(/images/original/water-background-1370w-605h.png',
     },
   },
-  plugins: [],
 };
