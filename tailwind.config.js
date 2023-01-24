879b554e274654c4165cbeffccb565bf0e04c9cb
Ashlyn Knox <ashlyn@lilyx.io>
2023-01-23 17:19:19 -0700

    adjustments to colors and custom spacing theme extend

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,13 +7,15 @@ module.exports = {
     colors: {
       black: '#000000',
       blue: {
-        100: '#D698FF',
+        50: '#EDFDFF',
+        100: '#BEDDF9',
         300: '#83F1FF',
         500: '#1ECCFF',
         700: '#009FFF',
         900: '#0D569B',
       },
       'deep-purple': {
+        50: '#F5F4FA',
         100: '#E1C5FF',
         300: '#B39ADD',
         500: '#8870B0',
@@ -21,23 +23,37 @@ module.exports = {
         900: '#37255D',
       },
       gray: {
-        100: '#514C5C',
+        50: '#F7F6F9',
+        100: '#D3CDDF',
         300: '#A5A0B1',
         500: '#7A7485',
-        700: '#A5A0B1',
+        700: '#514C5C',
         900: '#2B2735',
       },
-
-      purple: '#892CA0',
-      white: '#FFFFFFF',
+      purple: {
+        50: '#FBF5F3',
+        100: '#F7EAFD',
+        300: '#E3B4F3',
+        500: '#BD58DD',
+        /*Brand Purple */
+        700: '#892CA0',
+        900: '#5F246B',
+      },
+      white: '#FFFFFF',
     },
     fontFamily: {
       display: ['Montserrat', 'ui-sans-serif', 'system-ui', 'sans-serif'],
       text: ['"Source Sans Pro"', 'ui-sans-serif', 'system-ui', 'sans-serif'],
       mono: ['"Source Code Pro"', 'ui-monospace', 'monospace'],
     },
-    backgroundImage: {
-      'waterTexture': 'url(/images/original/water-background-1370w-605h.png',
+    extend: {
+      backgroundImage: {
+        'water-texture': "url('/images/original/water-background-1370w-605h.png')",
+      },
+      spacing: {
+        '98': '26rem',
+        '100': '30rem',
+      },
     },
   },
 };
