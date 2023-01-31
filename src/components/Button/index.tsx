fb34cd1beb08e9d5c2745cdbacc9a0ed3187155b
Ashlyn Knox <ashlyn@lilyx.io>
2023-01-31 16:19:42 -0700

    button variations
    Signed-off-by: Ashlyn Knox <ashlyn@lilyx.io>

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,10 +1,34 @@
 import React from 'react';
 
-export default function Button({ variant }): JSX.Element {
+function ButtonBase(props) {
+  const { as = 'button', buttonStyles, ...restProps } = props;
+  const Element = as;
   return (
-    <button
-      className={`rounded-md ${variant} px-6 py-1 font-medium transition duration-150 ease-in-out hover:bg-purple-900 hover:shadow-sm lg:px-8`}>
-      Join Meeting
-    </button>
+    <Element
+      {...restProps}
+      className={`my-2 block max-w-fit rounded-md px-6 py-2 font-semibold transition duration-150 ease-in-out hover:no-underline hover:shadow-md ${buttonStyles}`}
+    />
   );
 }
+
+function Button(props) {
+  const { text, variant = 'solid', ...restProps } = props;
+  if (variant === 'outline') {
+    return (
+      <ButtonBase
+        {...restProps}
+        buttonStyles="text-purple-700 outline outline-2   hover:bg-gray-50 hover:text-purple-900">
+        {text}
+      </ButtonBase>
+    );
+  }
+  return (
+    <ButtonBase
+      {...restProps}
+      buttonStyles="bg-purple-700 text-white py-3 px-7 hover:bg-purple-900 hover:text-white hover:no-underline hover:shadow-md">
+      {text}
+    </ButtonBase>
+  );
+}
+
+export default Button;
