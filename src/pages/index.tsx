f51852bf22d2423ec931ff62173efa24d1556158
Ashlyn Knox <ashlyn@lilyx.io>
2023-01-31 17:34:09 -0700

    testing buttons and cards on index.tsx page
    Signed-off-by: Ashlyn Knox <ashlyn@lilyx.io>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import Layout from '@theme/Layout';
 import PageHeader from '../components/PageHeader';
 import Card from '../components/Card';
+import Button from '../components/Button';
+import Link from '../components/Link';
 
 const headerInfo = {
   title: 'The daemonless container manager',
@@ -15,8 +17,31 @@ export default function Home(): JSX.Element {
       <main>
         <PageHeader title={headerInfo.title} description={headerInfo.description} />
         <div className="my-12 flex justify-center">
-          <Card />
-          <Card />
+          <Card
+            title="card one"
+            subtitle={
+              <>
+                <strong>1st Tuesday</strong> every month
+              </>
+            }
+            details="11 AM US ET / 5 PM CET"
+            text={
+              <>
+                This meeting is used to show demos for or to have general discussions about Podman or other related
+                container technologies. It is also used to make announcements about Podman and the other projects in the{' '}
+                <Link text="Containers Repository" url="https://github.com" /> on GitHub.
+              </>
+            }
+          />
+          <Card
+            title="card two"
+            subtitle={
+              <>
+                <strong>2nd Wednesday</strong> every month
+              </>
+            }
+            details="11 AM US ET / 5 PM CET"
+          />
         </div>
       </main>
     </Layout>
