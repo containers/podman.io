b9c3f367056500dd8b6c7e688511724a01fd776d
Ashlyn Knox <ashlyn@lilyx.io>
2023-01-31 17:33:17 -0700

    mid refactoring card for prop usage
    Signed-off-by: Ashlyn Knox <ashlyn@lilyx.io>

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,46 +1,43 @@
 import React from 'react';
 import Link from '../Link';
+import Button from '../Button';
 
-function Button({ text, url, variant }) {
-  if (variant === 'outline') {
-    return (
-      <a
-        href={url}
-        className="block rounded-md px-6 py-2 font-semibold text-purple-700 outline outline-2 -outline-offset-1 transition duration-150 ease-in-out hover:bg-gray-50 hover:text-purple-900 hover:no-underline hover:shadow-md">
-        {text}
-      </a>
-    );
-  }
+function CardHeader(props) {
+  const { title, subtitle, details } = props;
   return (
-    <a
-      href={url}
-      className="block rounded-md bg-purple-700 py-2 px-6 font-semibold text-white transition duration-150 ease-in hover:bg-purple-900 hover:text-white hover:no-underline hover:shadow-md">
-      {text}
-    </a>
+    <div className=" mx-2">
+      <h3 className="mb-3 font-bold text-gray-700">{title}</h3>
+      <p className="text-gray-700">{subtitle}</p>
+      <p className="text-gray-700">{details}</p>
+    </div>
+  );
+}
+function CardBody({ text }) {
+  return (
+    <div className="my-6 mx-2 lg:my-8">
+      <p className="max-w-sm text-gray-700">{text}</p>
+    </div>
   );
 }
 
+// TODO: must be able to assign individual link and inner text to each button at the page level
+function CardButtons(props) {
+  const { as, text, variant } = props;
+  // take button data and iterate through it to fill buttons
+  return (
+    <div className="mx-2 mb-4 flex justify-center gap-2 lg:mb-8">
+      <Button as="a" text="Join Meeting" url="" />
+      <Button as="a" variant="outline" text="Meeting Agenda" url="" />
+    </div>
+  );
+}
 export default function Card(props): JSX.Element {
+  const { ...passProps } = props;
   return (
     <article className="m-4 rounded-md p-4 shadow-xl lg:m-2">
-      <div className=" mx-2">
-        <h3 className="mb-3 font-bold text-gray-700">Podman Community meeting</h3>
-        <p className="text-gray-700">
-          <strong>1st Tuesday</strong> every month
-        </p>
-        <p className="text-gray-700">11 AM US ET / 5 PM CET</p>
-      </div>
-      <div className="my-6 mx-2 lg:my-8">
-        <p className="max-w-sm text-gray-700">
-          This meeting is used to show demos for or to have general discussions about Podman or other related container
-          technologies. It is also used to make announcements about Podman and the other projects in the{' '}
-          <Link text="Containers Repository" /> on GitHub.
-        </p>
-      </div>
-      <div className="mx-2 mb-4 flex justify-center gap-2 lg:mb-8">
-        <Button variant="" text="Join Meeting" url="" />
-        <Button variant="outline" text="Meeting Agenda" url="" />
-      </div>
+      <CardHeader {...passProps} />
+      <CardBody {...passProps} />
+      <CardButtons {...passProps} />
     </article>
   );
 }
