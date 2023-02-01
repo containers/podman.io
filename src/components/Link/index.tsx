078e7b36f5921b9a4e6758cfa8731dfe137f7247
Ashlyn Knox <ashlyn@lilyx.io>
2023-01-31 17:33:31 -0700

    link styles
    Signed-off-by: Ashlyn Knox <ashlyn@lilyx.io>

--- src/components/Link/index.tsx
+++ src/components/Link/index.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
 
-export default function Link(props): JSX.Element {
-  return <a href={props.url}>{props.text}</a>;
+export default function Link({ text, url }): JSX.Element {
+  return (
+    <a
+      href={url}
+      className="cursor-pointer text-blue-700 underline underline-offset-4 transition duration-150 ease-in hover:text-purple-700 dark:text-blue-900">
+      {text}
+    </a>
+  );
 }
