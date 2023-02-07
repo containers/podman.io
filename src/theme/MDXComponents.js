1a23396d423b5e3e05cccc527111a37bd14d66d8
Ashlyn Knox <ashlyn@lilyx.io>
2023-02-06 18:20:07 -0700

    hero auto import
    Signed-off-by: Ashlyn Knox <ashlyn@lilyx.io>

--- src/theme/MDXComponents.js
+++ src/theme/MDXComponents.js
@@ -3,6 +3,7 @@ import MDXComponents from '@theme-original/MDXComponents';
 import Main from '@site/src/components/layout/Main';
 import Section from '@site/src/components/layout/Section';
 import CardGrid from '@site/src/components/layout/CardGrid';
+import HeroHeader from '@site/src/components/HeroHeader';
 // Element Components
 import Card from '@site/src/components/Card';
 export default {
@@ -12,4 +13,5 @@ export default {
   section: Section,
   cardGrid: CardGrid,
   card: Card,
+  hero: HeroHeader,
 };
