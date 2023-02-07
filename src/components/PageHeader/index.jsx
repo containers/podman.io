f1c4b4d1b28168903af48daa4ceb6d3c27dff12c
Ashlyn Knox <ashlyn@lilyx.io>
2023-02-07 16:56:11 -0700

    page header component
    Signed-off-by: Ashlyn Knox <ashlyn@lilyx.io>

---
+++ src/components/PageHeader/index.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+const TextBox = ({ grid, display, layout, title = 'Default Title', description = 'Just some Text' }) => {
+  return (
+    <div className={`${grid} ${display} ${layout}`}>
+      <h1 className="mb-6 max-w-sm text-purple-700 dark:text-purple-500 lg:max-w-lg ">{title}</h1>
+      <p className="leading-relaxed">{description}</p>
+    </div>
+  );
+};
+const Image = ({
+  grid,
+  display,
+  layout,
+  image = { src: '/images/raw/podman-2-196w-172h.png', alt: 'Podman Logo' },
+}) => {
+  return (
+    <div>
+      <img src={image.src} alt={image.alt} className={`${grid} ${display} ${layout}`} />
+    </div>
+  );
+};
+export default function PageHeader({ title, description, image, children }) {
+  return (
+    <header className="h-5/6 xl:h-96">
+      <div className="container grid h-full content-center justify-items-center gap-3  md:grid-cols-2">
+        <TextBox title={title} description={description} layout="mt-12 lg:mt-0" />
+        <Image image={image} layout="mb-8 lg:mb-0" />
+        <div>{children}</div>
+      </div>
+    </header>
+  );
+}
