072e99d15c4bc775a6d069e3bbb5f7ca83a9a0ba
Ashlyn Knox <ashlyn@lilyx.io>
2023-02-06 18:19:48 -0700

    setting up both jsx and mdx examples
    Signed-off-by: Ashlyn Knox <ashlyn@lilyx.io>

---
+++ src/pages/demo.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Layout from '@theme/Layout';
+import HeroHeader from '../components/HeroHeader';
+
+const header = {
+  title: 'The best free & open source container tools',
+  subtitle:
+    'Manage containers, pods, and images with Podman. Seamlessly work with containers and Kubernetes from your local environment.',
+};
+
+export default function Demo() {
+  return (
+    <Layout>
+      <HeroHeader title={header.title} subtitle={header.subtitle} />
+    </Layout>
+  );
+}
