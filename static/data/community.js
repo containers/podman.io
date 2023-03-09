e9f7cbf142b8d61ef4aff5d3cfe7b55042a8ce54
Ashlyn <77300739+lilyx13@users.noreply.github.com>
2023-03-09 13:32:40 -0700

    community page data
    Signed-off-by: Ashlyn <ashlyn@lilyx.io>

--- static/data/community.js
+++ static/data/community.js
@@ -52,3 +52,44 @@ const communityChat = {
     },
   ],
 };
+
+const communityMeetings = {
+  title: 'Podman Community Meetings',
+  subtitle:
+    'Many of the maintainers for the Podman project attend both of these meetings, so it’s a great chance for community members like you to ask them questions or address concerns directly. If you have a topic that you’d like to propose for either meeting, please send a note to the Mailing List.',
+  cards: [
+    {
+      title: 'Podman Community Meeting',
+      subtitle:
+        'This meeting is used to show demos for or to have general discussions about Podman or other related container technologies. It is also used to make announcements about Podman and the other projects in the Containers repository on GitHub.',
+      date: '1st Tuesday every month',
+      timeZone: '11 AM US ET /5 PM CET',
+      buttons: [
+        { text: 'Join Meeting', path: '#' },
+        { text: 'Meeting Agenda', path: '#' },
+      ],
+    },
+    {
+      title: 'Podman Community Cabal',
+      subtitle:
+        'The focus of the cabal meeting is the planning and discussion of possible future changes to Podman or the related Containers projects and discussing any outstanding issues that might need solving.',
+      date: '3rd Thursday every month',
+      timeZone: '11 AM US ET /5 PM CET',
+      buttons: [
+        { text: 'Join Meeting', path: '#' },
+        { text: 'Meeting Agenda', path: '#' },
+      ],
+    },
+  ],
+  // TODO: Past Meetings - find out where this data is stored and if it can be auto updated on the site
+};
+
+const mailingList = {
+  title: 'Mailing List',
+  subtitle: 'The Podman Mailing list is available for your questions, concerns or comments about Podman.',
+  browseInfo: {
+    title: 'Browse the mailing list',
+    subtitle:
+      'Simply visit the Podman mailing list website to browse or search previous postings to the Podman mailing list.',
+  },
+};
