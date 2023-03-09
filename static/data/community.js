65c54ecbdce496ec276517b0dbb59b7f2d0b3c00
Ashlyn <77300739+lilyx13@users.noreply.github.com>
2023-03-09 14:52:10 -0700

    community page data complete
    Signed-off-by: Ashlyn <ashlyn@lilyx.io>

--- static/data/community.js
+++ static/data/community.js
@@ -56,7 +56,7 @@ const communityChat = {
 const communityMeetings = {
   title: 'Podman Community Meetings',
   subtitle:
-    'Many of the maintainers for the Podman project attend both of these meetings, so it’s a great chance for community members like you to ask them questions or address concerns directly. If you have a topic that you’d like to propose for either meeting, please send a note to the Mailing List.',
+    "Many of the maintainers for the Podman project attend both of these meetings, so it's a great chance for community members like you to ask them questions or address concerns directly. If you have a topic that you’d like to propose for either meeting, please send a note to the Mailing List.",
   cards: [
     {
       title: 'Podman Community Meeting',
@@ -92,4 +92,79 @@ const mailingList = {
     subtitle:
       'Simply visit the Podman mailing list website to browse or search previous postings to the Podman mailing list.',
   },
+  subscribeInfo: {
+    title: 'Subscribe or post to the mailing list',
+    subtitle:
+      'Simply visit <a href="#">the Podman mailing list</a> website to browse or search previous postings to the Podman mailing list.',
+    options: [
+      {
+        title: 'Option 1',
+        subtitle: 'Send an email to <a href="#">podman-join@lists.podman.io with the word "Subscribe" in the subject.',
+        button: {
+          text: 'Send email',
+          path: '#',
+        },
+      },
+      {
+        title: 'Option 2',
+        subtitle: 'Enter your email at the bottom of the mailing list sign up page, and hit the "Subscribe" button.',
+        button: {
+          text: 'Sign up page',
+          path: '#',
+        },
+      },
+    ],
+    description:
+      "Regardless of which method you use, a confirmation email will be sent to you. After you reply back to that confirmation email, you'll then be able to send mail directly to <a href='#'>podman@lists.podman.io</a>. You can then also go to <a href='#'>the list's web page</a> and manage your subscription.",
+  },
+};
+
+const submittingIssues = {
+  title: 'Submitting Issues & Pull Requests',
+  subtitle:
+    'The following is a quick cheat-sheet of sorts on how to submit issues and pull requests to the Podman project. For the most up-to-date and more comprehensive information, please take a look at <a href="#">CONTRIBUTING.md</a> in the Podman repo.',
+  sections: [
+    {
+      title: 'Submitting Issues',
+      subtitle: "Don't include private / sensitive info in issues!",
+      sections: [
+        {
+          text: '<strong>Before reporting an issue</strong>, <a href="#">check our backlog of open issues</a> to see if someone else has already reported it. If so:',
+          checkList: [
+            'Feel free to add your scenario, or additional information, to the discussion.',
+            'Subscribe to the issue to be notified when it is updated.',
+          ],
+          button: {
+            text: 'Check Open Issues',
+            path: '#',
+          },
+        },
+        {
+          text: "<strong>If you find a new issue</strong>, we'd love to hear about it! The most important aspect of a bug report is that it includes enough information for us to reproduce it. So, please:",
+          checkList: [
+            'Include as much detail as possible',
+            "Try to remove any extra stuff that doesn't really relate to the issue itself",
+          ],
+          button: {
+            text: 'File a New Issue',
+            path: '#',
+          },
+        },
+      ],
+    },
+    {
+      title: 'Submitting Pull Requets',
+      description:
+        "No Pull Request (PR) is too small! Typos, additional comments in the code, new test cases, bug fixes, new features, more documentation, ... it's all welcome! While bug fixes can first be identified via an \"issue\", that is not required. It's ok to just open up a PR with the fix, but make sure you include the same information you would have included in an issue - like how to reproduce it. PRs for new features should include some background on what use cases the new code is trying to address. When possible and when it makes sense, try to break-up larger PRs into smaller ones - it's easier to review smaller code changes. But only if those smaller ones make sense as stand-alone PRs. Regardless of the type of PR, all PRs should include:",
+      checkList: [
+        'Well-documnented code changes.',
+        'Additional testcases. Ideally m they should fail w/o your code change applied.',
+        'Documentation changes.',
+      ],
+      button: {
+        text: 'More PR Submission Details',
+        path: '#',
+      },
+    },
+  ],
 };
