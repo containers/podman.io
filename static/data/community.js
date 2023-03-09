2983e54a216eb3bde5b4234e1fc9952db0fe6795
Ashlyn <77300739+lilyx13@users.noreply.github.com>
2023-03-08 18:09:26 -0700

    community page data 2 sections
    Signed-off-by: Ashlyn <ashlyn@lilyx.io>

---
+++ static/data/community.js
@@ -0,0 +1,54 @@
+const header = {
+  title: 'Community',
+  subtitle:
+    'We want your feedback, issues, patches, and involvement in the development of Podman. **Chat** with us on Slack, IRC, or on our **mailing list**. Submit **issues & pull requests** (see our [CONTRIBUTING guide]() on how.) Participate in one of our twice-monthly community meetings. You are welcome in our community!',
+  image: 'images/raw/podman-2-196w-172h.png',
+  banner: {
+    text: 'To help ensure all feel welcome in the Podman community, we expect all who participate to adhere to our [Code of Conduct]()',
+    link: {
+      text: 'Code of Conduct',
+      path: 'https://github.com/containers/common/blob/main/CODE-OF-CONDUCT.md',
+      icon: 'fa6-regular:handshake',
+    },
+  },
+};
+
+const communityChat = {
+  title: 'Chat with the Podman community',
+  subtitle:
+    'The Podman developerss are generally around during CEST and Eastern Time business hours, so please be patient if you’re in another time zone!',
+  links: [
+    {
+      text: '#podman:matrix.org',
+      path: '#',
+      image: {
+        path: '#',
+        alt: 'Element Matrix Logo',
+      },
+    },
+    {
+      text: '#podman on libera.chat',
+      path: '#',
+      image: {
+        path: '',
+        alt: 'IRC Logo',
+      },
+    },
+    {
+      text: 'Podman Discord',
+      path: '#',
+      image: {
+        path: '#',
+        alt: 'Discord Logo',
+      },
+    },
+    {
+      text: '#CRIO on Kubernetes',
+      path: '#',
+      image: {
+        path: '#',
+        alt: 'Slack Logo',
+      },
+    },
+  ],
+};
