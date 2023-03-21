ee25b8bdea9b882ca901013f30cb3712f00d58a9
Ashlyn <ashlyn@lilyx.io>
2023-03-20 18:24:05 -0600

    data setup for coloring book

---
+++ src/components/ColoringBookSection/index.jsx
@@ -0,0 +1,27 @@
+const data = {
+  title: 'Have fun coloring and learn about Podman!',
+  description:
+    "A decentralized team of open source container tool superheroes comes to the rescue when an asteroid storm threatens the planet. Learn about each tool—Podman, CRI-O, Buildah, Skopeo, and OpenShift—as they redesign the planet's protective shields' container deployment to protect Earth.",
+  featureImage: {
+    src: '',
+    alt: 'Container Commandos coloring book cover',
+  },
+  images: [
+    {
+      src: 'images/raw/comic-idea-213w-240h.png',
+      alt: 'A coloring page on container processes',
+    },
+    {
+      src: 'images/raw/comic-ships-205w-246h.png',
+      alt: 'A coloring page that features deployment',
+    },
+    {
+      src: 'images/raw/comic-the-end-217w-276h.png',
+      alt: 'A summary coloring page',
+    },
+    {
+      src: '',
+      alt: 'A coloring page that describes what podman is',
+    },
+  ],
+};
