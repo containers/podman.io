8738e85bd430c3ba78ceec7a50df03c15d4435bb
lilyx13 <ashlyn@lilyx.io>
2023-03-24 16:40:41 -0600

    removed unneeded files
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>

--- src/components/layout/Main/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-//TODO: render props for HeroHeader or PageHeader built in
-// NOTE: no x margin or padding, this needs to allow full width sections
-export default function Main({ children }) {
-  return <main className="mx-auto h-screen">{children}</main>;
-}
