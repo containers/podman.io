6f2dfac832abf2738c39eaac13a9b9d99cb59521
lilyx13 <ashlyn@lilyx.io>
2023-03-24 16:46:19 -0600

    button notes on refactor changes
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>

---
+++ src/components/utilities/Button/_index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+type ButtonProps = {
+  text: string;
+  bgColor?: 'bg-purple-700 dark:bg-purple-500';
+  outline?: false;
+};
+
+/** Button
+ * TODO: simplify code, this approach is a hasle and since separating button and link, the complexity is unneeded
+ * TODO: Variants: solid, outline
+ * TODO: add event Listener prop
+ */
+
+export default function Button() {}
