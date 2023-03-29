22ef2e6d5b3c7b91d4ba2d9e2de82939d2e4c6fc
lilyx13 <ashlyn@lilyx.io>
2023-03-28 18:39:41 -0600

    card refactor setup
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>

--- src/components/ui/Card/_index.tsx
+++ src/components/ui/Card/_index.tsx
@@ -1,16 +1,35 @@
 import React from 'react';
-
+import Button from '@site/src/components/utilities/Button';
+import ReactMarkdown from 'react-markdown';
 type CardProps = {
   title: string;
-  subtitle: string;
+  subtitle?: string;
+  details?: string;
   description: string;
+  image?: {
+    src: string;
+    alt: string;
+  };
+  bgColor?: string;
+  textColor?: string;
+  buttons: JSX.Element[];
 };
 
-/** Standard Card
- * accepts: title, subtitle, 2 links
- * TODO: discuss with team on having multiple cards or a single card with more options
- */
+/** Standard Card */
 
-export default function Card({ title, subtitle, description }: CardProps): JSX.Element {
-  return <article></article>;
+export default function Card({ title, subtitle, details, description, image, buttons }: CardProps): JSX.Element {
+  return (
+    <article>
+      <div className="mx-2 mt-4">
+        <h3 className="mb-3 font-bold text-gray-700 dark:text-gray-50">{title}</h3>
+        <ReactMarkdown children={subtitle} className="text-gray-700" />
+        <ReactMarkdown children={details} className="text-gray-700" />
+      </div>
+      <div className="my-6 mx-2 lg:my-8">
+        {/* Optional Image */}
+        <p className="max-w-sm text-gray-700 dark:text-gray-100">{description}</p>
+      </div>
+      {/* Buttons */}
+    </article>
+  );
 }
