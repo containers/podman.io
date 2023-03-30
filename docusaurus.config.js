dd7663761dde488d561d6609471c2d0cecd512f3
lilyx13 <ashlyn@lilyx.io>
2023-03-30 06:44:10 -0600

    path fix
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   tagline:
     'Podman is a daemonless container engine for developing, managing, and running OCI Containers on your Linux System',
   url: 'https://containers.github.io',
-  baseUrl: '/websites-new/',
+  baseUrl: '/website-new/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'images/favicon.ico',
