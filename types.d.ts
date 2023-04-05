0c61cfd2d9a750846728fbf200ceb0d61c09452c
lilyx13 <ashlyn@lilyx.io>
2023-04-04 18:39:41 -0600

    base types
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>

--- types.d.ts
+++ types.d.ts
@@ -1,4 +1,19 @@
-type ImageProps = {
-  src: string;
+type Link = {
+  text: string;
+  path: string | undefined;
+};
+
+type Image = {
+  path: string | undefined;
   alt: string;
+};
+
+type Button = {
+  text: string;
+  action: () => void | undefined;
+};
+
+type Colors = {
+  light: string | undefined;
+  dark: string | undefined;
 };
