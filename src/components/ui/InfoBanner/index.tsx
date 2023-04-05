191a8199628f546bdb74bd76c943ba5d49b79786
lilyx13 <ashlyn@lilyx.io>
2023-04-05 16:09:47 -0600

    lazy load for react-markdown component
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>

--- src/components/ui/InfoBanner/index.tsx
+++ src/components/ui/InfoBanner/index.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import ReactMarkdown from 'react-markdown';
+import React, { lazy, Suspense } from 'react';
+const ReactMarkdown = lazy(() => import('react-markdown'));
 import { Icon } from '@iconify/react';
 
 type BannerProps = {
@@ -40,7 +40,9 @@ export default function InfoBanner({
         {title ? (
           <div className="mx-auto text-center md:text-start lg:pl-4">
             <h3 className={`mx-auto mb-4 text-3xl font-bold ${titleColor}`}>{title}</h3>
-            <ReactMarkdown children={description} className={`mx-auto max-w-4xl leading-relaxed text-gray-700`} />
+            <Suspense fallback={<div>loading</div>}>
+              <ReactMarkdown children={description} className={`mx-auto max-w-4xl leading-relaxed text-gray-700`} />
+            </Suspense>
           </div>
         ) : (
           <ReactMarkdown children={description} className={`mx-auto leading-relaxed`} />
