f8142d0cc79fa99c1918b6f38540c308fb822b68
lilyx13 <ashlyn@lilyx.io>
2023-04-04 19:26:07 -0600

    Button as an example of optimised ts pattern
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>

--- src/components/utilities/Button/index.tsx
+++ src/components/utilities/Button/index.tsx
@@ -1,34 +1,35 @@
 import React from 'react';
 
-export default function Button({
-  as = 'a',
-  path,
-  method,
-  text,
-  icon,
-  secondary,
-  primaryColors = 'bg-purple-700 dark:bg-purple-500 text-white dark:text-gray-50 hover:bg-purple-900 hover:dark-purple-700',
-  secondaryColors = 'outline-purple-700 dark:outline-purple-500 text-purple-700 dark:text-purple-700 hover:bg-gray-50 hover:dark-bg-gray-100',
-}: ButtonProps) {
-  /** Element type */
-  const Element = as;
+type ButtonProps = Button & {
+  outline: boolean;
+  icon: string;
+  solidColors: string | undefined;
+  outlineColors: string | undefined;
+};
 
-  /** style variations */
+export default function Button(
+  {
+    as = 'a',
+    solidColors = 'bg-purple-700 dark:bg-purple-500 text-white dark:text-gray-50 hover:bg-purple-900 hover:dark-purple-700',
+    outlineColors = 'outline-purple-700 dark:outline-purple-500 text-purple-700 dark:text-purple-700 hover:bg-gray-50 hover:dark-bg-gray-100',
+  },
+  props: ButtonProps,
+) {
   const baseStyles =
     'my-2 block max-w-fit cursor-pointer rounded-md px-6 py-2 font-semibold transition duration-150 ease-in-out hover:no-underline hover:shadow-md';
-  const variantStyles = secondary ? `bg-transparent outline outline-2 ${secondaryColors}` : `${primaryColors}`;
+  const variantStyles = props.outline ? `bg-transparent outline outline-2 ${outlineColors}` : `${solidColors}`;
 
   /** Render the component with style variations */
   if (as === 'button') {
     return (
-      <Element onClick={method} className={`${baseStyles} ${variantStyles}`}>
-        {icon ? icon : ''} {text}
-      </Element>
+      <button onClick={props.method} className={`${baseStyles} ${variantStyles}`}>
+        {props.icon ? props.icon : ''} {props.text}
+      </button>
     );
   }
   return (
-    <Element href={path} className={`${baseStyles} ${variantStyles}`}>
-      {icon ? icon : ''} {text}
-    </Element>
+    <a href={props.path} className={`${baseStyles} ${variantStyles}`}>
+      {props.icon ? props.icon : ''} {props.text}
+    </a>
   );
 }
--- types.d.ts
+++ types.d.ts
@@ -1,19 +1,21 @@
-type Link = {
-  text: string;
-  path: string | undefined;
-};
-
 type Image = {
   path: string | undefined;
   alt: string;
 };
 
-type Button = {
-  text: string;
-  action: () => void | undefined;
-};
-
 type Colors = {
   light: string | undefined;
   dark: string | undefined;
+};
+
+type Link = {
+  text: string;
+  path: string | undefined;
+};
+
+type Button = {
+  readonly text: string;
+  readonly as: 'a' | 'button';
+  readonly method: () => void | undefined;
+  readonly path?: string;
 };
