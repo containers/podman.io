bc8ca0d58a56633cc62a27bd579571dc2f62588c
lilyx13 <ashlyn@lilyx.io>
2023-04-06 17:50:08 -0600

    notes for get started page work
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>

--- src/pages/get-started.tsx
+++ src/pages/get-started.tsx
@@ -5,7 +5,18 @@ import { header } from '@site/static/data/get-started';
 export default function Community() {
   return (
     <Layout>
+      {/* TODO: Add List to the header using ListItem Component */}
       <PageHeader title={header.title} description={header.subtitle} />
+      {/* Getting Help */}
+      {/* TODO: Check if code block + copy already built into docusaurus' components */}
+      {/* Searching, Pulling, and listing images */}
+      {/* TODO: Add content to data file */}
+      {/* TODO: Animate slide in text boxes on side, use shadowing */}
+      {/* TODO: See about using the infobox component for this and ones in following section */}
+      {/* TODO: also follow up with terminal component */}
+      {/* Running a container and listing running containers */}
+      {/* TODO: Add content to data file */}
+      {/* TODO: See if they want the Want to learn more section on this page too  */}
     </Layout>
   );
 }
