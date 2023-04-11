48965124d76735aaabd23e7166d334d74b6c02af
lilyx13 <ashlyn@lilyx.io>
2023-04-11 11:00:02 -0600

    article card html
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>

--- src/components/ui/ArticleCard/index.tsx
+++ src/components/ui/ArticleCard/index.tsx
@@ -1,5 +1,31 @@
 import React from 'react';
+import Link from '@site/src/components/utilities/Link';
+type ArticleCard = Card & {
+  author: Link;
+  date: string;
+  image: Image;
+};
 
-export default function ArticleCard() {
-  return <article></article>;
+const PublishDate = ({ date }: { date: string }) => {
+  return (
+    <div>
+      <p>{date}</p>
+    </div>
+  );
+};
+
+export default function ArticleCard(props: ArticleCard) {
+  return (
+    <article>
+      <div>
+        <PublishDate date={props.date} />
+        <img {...props.image} />
+      </div>
+      <div>
+        <h3>{props.title}</h3>
+        <p>{props.subtitle}</p>
+        <Link {...props.author} />
+      </div>
+    </article>
+  );
 }
