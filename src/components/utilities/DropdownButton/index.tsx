b7940c9b5f22b3b605acb1607c373b41fe5022ce
lilyx13 <ashlyn@lilyx.io>
2023-04-12 14:41:16 -0600

    js toggle dropdown list for better style control
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>

--- src/components/utilities/DropdownButton/index.tsx
+++ src/components/utilities/DropdownButton/index.tsx
@@ -1,24 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Icon } from '@iconify/react';
 
 type DropdownProps = {
   selectName: string;
-  initialText: string;
+  buttonText: string;
   options: string[];
 };
 
 export default function DropdownButton(props: DropdownProps) {
+  const [open, setOpen] = useState<boolean>(false);
+  const toggleDropdown = state => {
+    setOpen(!state);
+  };
+  console.log(open);
   return (
     <div>
-      <select
-        name={props.selectName}
-        id={props.selectName}
-        className="appearance-none rounded-md bg-white px-4 py-2 hover:bg-purple-700 hover:text-white active:bg-white active:text-gray-900 active:shadow-lg">
-        <option className="">{props.initialText}</option>
-        {props.options.map(item => {
-          return <option value="">{item}</option>;
-        })}
-      </select>
+      <button
+        id="dropdownButton"
+        data-dropdown-toggle="dropdown"
+        onClick={e => toggleDropdown(open)}
+        className="flex items-center gap-2 rounded-md bg-white px-4 py-2 font-bold hover:bg-purple-700 hover:text-white focus:shadow-md dark:text-purple-900 dark:hover:text-white">
+        <span>{props.buttonText}</span>
+        <Icon icon="ion:caret-down-outline" />
+      </button>
+      {open && (
+        <div id="dropdown">
+          <ul aria-labelledby="dropdownButton">
+            {props.options.map((listItem, index) => {
+              return <li key={index}>{listItem}</li>;
+            })}
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
