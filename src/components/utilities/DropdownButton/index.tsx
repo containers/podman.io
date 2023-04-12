0db9298e3e6e4d124b70dd878880faa2d84d94e9
lilyx13 <ashlyn@lilyx.io>
2023-04-12 17:36:52 -0600

    render a component or string dropdownbutton
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>

--- src/components/utilities/DropdownButton/index.tsx
+++ src/components/utilities/DropdownButton/index.tsx
@@ -4,7 +4,7 @@ import { Icon } from '@iconify/react';
 type DropdownProps = {
   selectName: string;
   buttonText: string;
-  options: string[];
+  Options: (React.ReactNode | string)[];
 };
 
 export default function DropdownButton(props: DropdownProps) {
@@ -12,22 +12,25 @@ export default function DropdownButton(props: DropdownProps) {
   const toggleDropdown = state => {
     setOpen(!state);
   };
-  console.log(open);
   return (
     <div>
       <button
         id="dropdownButton"
         data-dropdown-toggle="dropdown"
         onClick={e => toggleDropdown(open)}
-        className="flex items-center gap-2 rounded-md bg-white px-4 py-2 font-bold hover:bg-purple-700 hover:text-white focus:shadow-md dark:text-purple-900 dark:hover:text-white">
+        className="flex items-center gap-2 rounded-md  bg-white px-4 py-2 font-bold transition duration-150 ease-linear hover:bg-purple-700 hover:text-white focus:shadow-md dark:text-purple-900 dark:hover:text-white">
         <span>{props.buttonText}</span>
         <Icon icon="ion:caret-down-outline" />
       </button>
       {open && (
-        <div id="dropdown">
-          <ul aria-labelledby="dropdownButton">
-            {props.options.map((listItem, index) => {
-              return <li key={index}>{listItem}</li>;
+        <div id="dropdown" className="mt-2 max-w-fit rounded-md bg-white shadow-md dark:bg-gray-900">
+          <ul aria-labelledby="dropdownButton" className="ml-2 py-4">
+            {props.Options.map((ListItem, index) => {
+              return (
+                <li key={index} className="pl-2 pr-8 hover:bg-purple-700 hover:text-white">
+                  {typeof ListItem == 'string' ? { ListItem } : <ListItem />}
+                </li>
+              );
             })}
           </ul>
         </div>
