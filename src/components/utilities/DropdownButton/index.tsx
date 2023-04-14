3361da4dffcf361002b88723d250118d66924bf9
lilyx13 <ashlyn@lilyx.io>
2023-04-14 12:30:50 -0600

    partial set up to handle os detection better
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>

--- src/components/utilities/DropdownButton/index.tsx
+++ src/components/utilities/DropdownButton/index.tsx
@@ -2,8 +2,9 @@ import React, { useState } from 'react';
 import { Icon } from '@iconify/react';
 
 type DropdownProps = {
-  buttonText: string;
-  Options: (React.ReactNode | string)[];
+  text: string;
+  options?: (React.ReactNode | string)[];
+  option: React.ReactNode;
 };
 
 export default function DropdownButton(props: DropdownProps) {
@@ -18,18 +19,14 @@ export default function DropdownButton(props: DropdownProps) {
         data-dropdown-toggle="dropdown"
         onClick={e => toggleDropdown(open)}
         className="my-2 flex items-center gap-2 rounded-md bg-white px-4 py-2 font-bold text-purple-700 transition duration-150 ease-linear hover:bg-purple-700 hover:text-white focus:shadow-md dark:text-purple-900 dark:hover:text-white">
-        <span>{props.buttonText}</span>
+        <span>{props.text}</span>
         <Icon icon="ion:caret-down-outline" />
       </button>
       {open && (
-        <div id="dropdown" className="absolute  mt-2 max-w-fit rounded-md bg-white shadow-md dark:bg-gray-900">
-          <ul aria-labelledby="dropdownButton" className="ml-2 py-4">
-            {props.Options.map((ListItem, index) => {
-              return (
-                <li key={index} className="pl-2 pr-8 hover:bg-purple-700 hover:text-white">
-                  {typeof ListItem == 'string' ? { ListItem } : <ListItem />}
-                </li>
-              );
+        <div id="dropdown" className="absolute mt-2 max-w-fit rounded-md bg-white shadow-md dark:bg-gray-900">
+          <ul aria-labelledby="dropdownButton" className="">
+            {props.options.map((ListItem, index) => {
+              return <li key={index}>{ListItem}</li>;
             })}
           </ul>
         </div>
