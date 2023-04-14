c5270e216e5fe47f34e52098bc75d13b02f1239b
lilyx13 <ashlyn@lilyx.io>
2023-04-14 12:30:06 -0600

    styles for dropdown prop components and data moved to separate file
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>

---
+++ src/components/layout/HeroHeader/installOptions.ts
@@ -0,0 +1,41 @@
+const windows = {
+  title: 'Download for Windows',
+  subtitle: 'Install on Desktop',
+  icon: 'fa-brands:windows',
+  options: [],
+  path: 'https://github.com/containers/podman-desktop/releases/download/v0.13.0/podman-desktop-0.13.0-setup.exe',
+  other: {
+    path: 'https://podman-desktop.io/downloads',
+    text: 'Other Install Options',
+    subtext: '(Including Windows portable executable and other OS options)',
+    icon: 'material-symbols:arrow-circle-right-rounded',
+  },
+};
+const mac = {
+  title: 'Download for macOS',
+  subtitle: 'Universal *.dmg',
+  icon: 'fa-brands:apple',
+  options: [],
+  path: 'https://github.com/containers/podman-desktop/releases/download/v0.13.0/podman-desktop-0.13.0-universal.dmg',
+  other: {
+    path: 'https://podman-desktop.io/downloads',
+    text: 'Other Install Options',
+    subtext: '(Including macOS Intel & Arm builds and other Os options)',
+    icon: 'material-symbols:arrow-circle-right-rounded',
+  },
+};
+const linux = {
+  title: 'Download for Linux',
+  subtitle: 'Install on Desktop',
+  icon: 'fa-brands:linux',
+  options: [{ path: 'https://podman.io/getting-started/installation#installing-on-linux', text: 'CLI Install' }],
+  path: 'https://github.com/containers/podman-desktop/releases/download/v0.13.0/podman-desktop-0.13.0.flatpak',
+  other: {
+    path: 'https://podman-desktop.io/downloads',
+    text: 'Other Install Options',
+    subtext: '(Including binary tar.gz and other OS options)',
+    icon: 'material-symbols:arrow-circle-right-rounded',
+  },
+};
+
+export { windows, mac, linux };
