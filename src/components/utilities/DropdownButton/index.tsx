6ac6304eccf334e4c8d888f3d70517bc3eca2f2e
lilyx13 <ashlyn@lilyx.io>
2023-04-17 15:51:20 -0600

    proper toggle behaviour
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>

--- src/components/utilities/DropdownButton/index.tsx
+++ src/components/utilities/DropdownButton/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Icon } from '@iconify/react';
 
 type DropdownProps = {
@@ -7,30 +7,40 @@ type DropdownProps = {
   option: React.ReactNode;
 };
 
-export default function DropdownButton(props: DropdownProps) {
-  const [open, setOpen] = useState<boolean>(false);
-  const toggleDropdown = state => {
-    setOpen(!state);
-  };
+function toggleDropdown(ref, handler) {
+  useEffect(() => {
+    const listener = event => {
+      if (!ref.current || ref.current.contains(event.target)) {
+        return;
+      }
+      handler(event);
+    };
+    document.body.addEventListener('mousedown', listener);
+    document.body.addEventListener('touchstart', listener);
+    return () => {
+      document.body.removeEventListener('mousedown', listener);
+      document.body.removeEventListener('touchstart', listener);
+    };
+  }, [ref, handler]);
+}
 
+function DropdownButton(props: DropdownProps) {
+  const dropdownRef = useRef();
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  toggleDropdown(dropdownRef, () => setIsOpen(false));
   return (
-    <div>
+    <div ref={dropdownRef}>
       <button
-        id="dropdownButton"
         data-dropdown-toggle="dropdown"
-        onClick={e => toggleDropdown(open)}
+        onClick={() => setIsOpen(prev => !prev)}
         className="my-2 flex items-center gap-2 rounded-md bg-white px-4 py-2 font-bold text-purple-700 transition duration-150 ease-linear hover:bg-purple-700 hover:text-white focus:shadow-md dark:text-purple-900 dark:hover:text-white">
         <span>{props.text}</span>
         <Icon icon="ion:caret-down-outline" />
       </button>
-      {open && (
-        <div id="dropdown" className="absolute mt-2 max-w-fit rounded-md bg-white shadow-md dark:bg-gray-900">
-          {/* {props.options.map((ListItem, index) => {
-            return <li key={index}>{ListItem}</li>;
-          })} */}
-          {props.option}
-        </div>
+      {isOpen && (
+        <div className="absolute mt-2 max-w-fit rounded-md bg-white shadow-md dark:bg-gray-900">{props.option}</div>
       )}
     </div>
   );
 }
+export default DropdownButton;
