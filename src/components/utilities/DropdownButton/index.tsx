c28911e88ea29052fe20b8ddfc1548da1525fcfe
Ashlyn <77300739+lilyx13@users.noreply.github.com>
2023-04-17 16:53:21 -0600

    Dropdown Button CSR (#24)
    * init dropdown button file
    
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>
    
    * removed outline on secondary buttons
    
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>
    
    * dropdown button using select element
    
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>
    
    * js toggle dropdown list for better style control
    
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>
    
    * fixed text color hover on button
    
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>
    
    * render a component or string dropdownbutton
    
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>
    
    * dropdown button on hero header basic implementation
    
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>
    
    * demo of options button on home page feat os detection
    
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>
    
    * adding .idea to gitignore for jetbrains
    
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>
    
    * hero button dropdown data
    
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>
    
    * prop component on heroheader and edits to dropdownbutton
    
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>
    
    * styles for dropdown prop components and data moved to separate file
    
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>
    
    * partial set up to handle os detection better
    
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>
    
    * dropdown item dark mode hover color
    
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>
    
    * WIP passing data and rendering based on user OS
    
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>
    
    * brute force dropdown button detection working
    
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>
    
    * brute force application of dropdownbutton in heroheader
    
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>
    
    * proper toggle behaviour
    
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>
    
    * removed unneeded props from type
    
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>
    
    * browserOnly used for rendering client side reactive code
    
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>
    
    ---------
    
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>

--- src/components/utilities/DropdownButton/index.tsx
+++ src/components/utilities/DropdownButton/index.tsx
@@ -1,36 +1,49 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+
 import { Icon } from '@iconify/react';
 
 type DropdownProps = {
   text: string;
-  options?: React.ReactNode | Link[];
   option: React.ReactNode;
 };
 
-export default function DropdownButton(props: DropdownProps) {
-  const [open, setOpen] = useState<boolean>(false);
-  const toggleDropdown = state => {
-    setOpen(!state);
-  };
+function toggleDropdown(ref, handler) {
+  useEffect(() => {
+    const listener = event => {
+      if (!ref.current || ref.current.contains(event.target)) {
+        return;
+      }
+      handler(event);
+    };
+    document.addEventListener('mousedown', listener);
+    document.addEventListener('touchstart', listener);
+    return () => {
+      document.removeEventListener('mousedown', listener);
+      document.removeEventListener('touchstart', listener);
+    };
+  }, [ref, handler]);
+}
 
+function DropdownButton(props: DropdownProps) {
+  const dropdownRef = useRef();
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  toggleDropdown(dropdownRef, () => setIsOpen(false));
   return (
-    <div>
+    <div ref={dropdownRef}>
       <button
-        id="dropdownButton"
         data-dropdown-toggle="dropdown"
-        onClick={e => toggleDropdown(open)}
+        onClick={() => setIsOpen(prev => !prev)}
+
         className="my-2 flex items-center gap-2 rounded-md bg-white px-4 py-2 font-bold text-purple-700 transition duration-150 ease-linear hover:bg-purple-700 hover:text-white focus:shadow-md dark:text-purple-900 dark:hover:text-white">
         <span>{props.text}</span>
         <Icon icon="ion:caret-down-outline" />
       </button>
-      {open && (
-        <div id="dropdown" className="absolute mt-2 max-w-fit rounded-md bg-white shadow-md dark:bg-gray-900">
-          {/* {props.options.map((ListItem, index) => {
-            return <li key={index}>{ListItem}</li>;
-          })} */}
-          {props.option}
-        </div>
+      {isOpen && (
+        <div className="absolute mt-2 max-w-fit rounded-md bg-white shadow-md dark:bg-gray-900">{props.option}</div>
       )}
     </div>
   );
 }
+
+export default DropdownButton;
+
