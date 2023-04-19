3a724b9b8e1d64df39dcc50d6dffeebc746d316f
lilyx13 <ashlyn@lilyx.io>
2023-04-18 19:57:36 -0600

    testimonial removed empty space
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>

--- src/components/ui/Testimonial/index.tsx
+++ src/components/ui/Testimonial/index.tsx
@@ -13,7 +13,7 @@ type TestimonialProps = {
 
 export default function Testimonial(props: TestimonialProps) {
   return (
-    <article className="my-4 max-w-sm  rounded-sm bg-white p-4 shadow-lg dark:bg-gray-900">
+    <article className="my-4 max-w-xs rounded-sm bg-white p-4 shadow-lg dark:bg-gray-900">
       <div className="flex items-center gap-2">
         <div>
           <div className="flex items-center gap-2">
