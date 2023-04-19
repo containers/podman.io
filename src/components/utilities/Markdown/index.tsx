d3d4746933324b3756942d1d199ecbe7a8118c53
lilyx13 <ashlyn@lilyx.io>
2023-04-19 15:38:24 -0600

    markdown wrapper component
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>

---
+++ src/components/utilities/Markdown/index.tsx
@@ -0,0 +1,25 @@
+import React, { lazy, Suspense } from 'react';
+import BrowserOnly from '@docusaurus/BrowserOnly';
+const ReactMarkdown = lazy(() => import('react-markdown'));
+
+interface Props {
+  text: string;
+  styles: string;
+}
+
+const fallBackComponent = () => {
+  return <p>text loading...</p>;
+};
+
+function Markdown({ text, styles }: Props): JSX.Element {
+  return (
+    <BrowserOnly>
+      {() => (
+        <Suspense fallback={fallBackComponent()}>
+          <ReactMarkdown children={text} className={styles} />
+        </Suspense>
+      )}
+    </BrowserOnly>
+  );
+}
+export default Markdown;
