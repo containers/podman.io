9fbc41040eb7d207325b718658ea5f248ca5ae53
lilyx13 <ashlyn@lilyx.io>
2023-04-20 17:42:11 -0600

    tab content component
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>

--- src/components/content/FeaturesCarousel/index.tsx
+++ src/components/content/FeaturesCarousel/index.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { library } from 'webpack';
 
 const FindTabContent = (): JSX.Element => {
   return (
@@ -33,6 +34,34 @@ const ShareTabContent = (): JSX.Element => {
   );
 };
 
+const TabContent = props => {
+  const { title, commands, image, description, index = 0 } = props;
+  const bgColor = () => {
+    const applyColors = (color, secondColor = 'gray') =>
+      `bg-gradient-to-br from-${color}-500 to-${color}-700 dark:from-${secondColor}-900 via-${color}-700 to-${color}-900`;
+    return index === 0 ? applyColors('purple') : index === 2 ? applyColors('blue') : 'bg-white dar:bg-gray-900';
+  };
+  return (
+    <section className={` ${bgColor}`}>
+      <div className="flex gap-4">
+        <div>
+          <h3>{title}</h3>
+          <ul>
+            {commands.map((command, index) => (
+              <li key={index}>{command}</li>
+            ))}
+          </ul>
+          <p>{description}</p>
+        </div>
+        <div className={`${index % 2 === 1 && `order-first`}`}>
+          {/* TODO: use codeblock if component isn't annoying */}
+          <img src={image.path} alt={image.text} />
+        </div>
+      </div>
+    </section>
+  );
+};
+
 const tabsData = [
   {
     label: 'Find',
@@ -56,16 +85,13 @@ const tabsData = [
   },
 ];
 
-interface TabProps {
-  label: string;
-}
 function Tab(props) {
   const { label, method, isActive } = props;
   return (
     <button
       onClick={method}
       className={`rounded-lg  p-4 shadow-md ${
-        isActive ? 'bg-gradient-radial from-purple-500 to-purple-700' : 'bg-white'
+        isActive ? 'bg-gradient-radial from-purple-500 to-purple-700 text-white' : 'bg-white'
       }`}>
       {label}
     </button>
@@ -77,8 +103,8 @@ function FeaturesCarousel() {
 
   // render content
   return (
-    <section className="mx-auto">
-      <div className="container flex justify-center gap-4">
+    <section>
+      <div>
         {/* Loop through Tab data and generate tab buttons for each */}
         {tabsData.map((tab, index) => {
           return (
