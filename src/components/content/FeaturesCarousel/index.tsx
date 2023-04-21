516db2613f91322fbe176a96a7ded43b53e5b7dc
Ashlyn <77300739+lilyx13@users.noreply.github.com>
2023-04-21 17:59:17 -0600

    Carousel secondary toggles and better colors (#31)
    * minimal code for carousel selector
    
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>
    
    * init custom code component for code box
    
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>
    
    * FeaturesCarousel component on features page
    
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>
    
    * active tab indicator
    
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>
    
    * key on learn articles loop
    
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>
    
    * tab content component
    
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>
    
    * more carousel tab selector style tweaks
    
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>
    
    * tab component logical cleanup
    
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>
    
    * carousel content vert text spacing
    
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>
    
    * removed ul and li from default styles
    
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>
    
    * carousel dynamic font and bg colors
    
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>
    
    * missed ul from main css
    
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>
    
    * carousel mobile responsive layout
    
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>
    
    * tab spacing tweaks
    
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>
    
    * color tweaks and extra toggles on carousel
    
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>
    
    ---------
    
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>

--- src/components/content/FeaturesCarousel/index.tsx
+++ src/components/content/FeaturesCarousel/index.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Icon } from '@iconify/react';
 import tabData from './data';
 
 const Tab = (props): JSX.Element => {
@@ -6,8 +7,7 @@ const Tab = (props): JSX.Element => {
   return (
     <button
       onClick={method}
-      className={`rounded-lg p-4 shadow-md transition duration-150 hover:bg-purple-700 hover:text-white dark:hover:bg-purple-900 dark:hover:text-gray-50 md:h-36 md:w-52
-       
+      className={`rounded-lg p-4 shadow-sm transition duration-150 hover:bg-purple-700 hover:text-white dark:hover:bg-purple-900 dark:hover:text-gray-50 md:h-36 md:w-56
       ${
         isActive
           ? 'bg-gradient-radial from-purple-500 to-purple-700 text-white dark:from-purple-700 dark:to-purple-900 dark:shadow-purple-900'
@@ -29,14 +29,7 @@ const Tab = (props): JSX.Element => {
 const TabContent = (props): JSX.Element => {
   const { title, commands, image, description, isActive } = props;
   return (
-    <section
-      className={`py-10 md:py-16 ${
-        isActive % 2 === 1
-          ? 'bg-white dark:bg-gray-900'
-          : isActive === 2
-          ? 'bg-gradient-to-br from-blue-300 via-blue-500 to-blue-900'
-          : 'bg-gradient-to-br from-purple-300 via-purple-700 to-purple-900'
-      }`}>
+    <>
       <div className="container flex flex-wrap justify-center gap-4 md:gap-12">
         <div className="max-w-sm">
           <h3
@@ -51,20 +44,23 @@ const TabContent = (props): JSX.Element => {
           </h3>
           <ul className="my-4 lg:my-12">
             {commands.map((command, index) => (
-              <li key={index} className="font-mono text-blue-500">
+              <li key={index} className={`font-mono  ${isActive == 2 ? 'text-white' : 'text-blue-500'}`}>
                 {command}
               </li>
             ))}
           </ul>
-          <p className={isActive % 2 === 0 ? 'text-white' : 'text-gray-900 dark:text-white'}>{description}</p>
+          <p className={isActive % 2 === 0 ? 'text-white dark:text-white' : 'text-gray-900 dark:text-white'}>
+            {description}
+          </p>
         </div>
         <div className={`${isActive % 2 === 1 && 'md:order-first'}`}>
           <img {...image} className="object-fill lg:h-[400px] lg:w-[600px]" />
         </div>
       </div>
-    </section>
+    </>
   );
 };
+
 function FeaturesCarousel() {
   const [activeTabIndex, setActiveTabIndex] = useState(0);
 
@@ -79,9 +75,36 @@ function FeaturesCarousel() {
           );
         })}
       </div>
-      <div>
+      <section
+        className={`flex py-10 md:py-16 ${
+          activeTabIndex % 2 === 1
+            ? 'bg-white dark:bg-gray-900'
+            : activeTabIndex === 2
+            ? 'bg-gradient-to-br from-blue-300 via-blue-500 to-blue-900'
+            : 'bg-gradient-to-br from-purple-300 via-purple-700 to-purple-900'
+        }`}>
+        <div className=" relative ml-20 self-center">
+          <button onClick={() => setActiveTabIndex(activeTabIndex > 0 ? activeTabIndex - 1 : tabData.length - 1)}>
+            <Icon
+              icon="fa-solid:arrow-circle-left"
+              className={`text-3xl transition duration-150 ease-linear hover:text-purple-900 dark:hover:text-purple-700  ${
+                activeTabIndex % 2 === 0 ? 'text-white' : 'text-gray-900 dark:text-gray-50'
+              }`}
+            />
+          </button>
+        </div>
         <TabContent {...tabData[activeTabIndex]} isActive={activeTabIndex} />
-      </div>
+        <div className="relative mr-20 self-center">
+          <button onClick={() => setActiveTabIndex(activeTabIndex < 3 ? activeTabIndex + 1 : 0)}>
+            <Icon
+              icon="fa-solid:arrow-circle-right"
+              className={`text-3xl transition duration-150 ease-linear hover:text-purple-900 dark:hover:text-purple-700 ${
+                activeTabIndex % 2 === 0 ? 'text-white' : 'text-gray-900 dark:text-gray-50'
+              }`}
+            />
+          </button>
+        </div>
+      </section>
     </section>
   );
 }
