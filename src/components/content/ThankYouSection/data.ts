35bb285043b479adaa09012de8fb3a2c0a7924c3
lilyx13 <ashlyn@lilyx.io>
2023-04-24 18:58:59 -0600

    thank you section data
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>

---
+++ src/components/content/ThankYouSection/data.ts
@@ -0,0 +1,42 @@
+export default [
+  {
+    label: 'Red Hat',
+    href: 'https://www.redhat.com/',
+    src: 'logos/raw/red-hat-120w-77h.png',
+  },
+  {
+    label: 'Debian',
+    href: 'https://www.debian.org/',
+    src: 'logos/raw/debian-68w-90h.png',
+  },
+  {
+    label: 'Amadeus',
+    href: 'https://www.amadeus.com/',
+    src: 'logos/raw/amadeus-171w-22h.png',
+  },
+  {
+    label: 'Motorola',
+    href: 'https://www.motorola.com/',
+    src: 'logos/raw/motorola-221w-105h.png',
+  },
+  {
+    label: 'Suse',
+    href: 'https://www.suse.com',
+    src: 'logos/raw/suse-167w-30h.png',
+  },
+  {
+    label: 'NTT',
+    href: 'https://www.global.ntt',
+    src: 'logos/raw/ntt-145w-50h.png',
+  },
+  {
+    label: 'IBM',
+    href: 'https://www.ibm.com',
+    src: 'logos/raw/ibm-92w-37h.png',
+  },
+  {
+    label: 'Fujitsu',
+    href: 'https://www.fujitsu.com',
+    src: 'logos/raw/fujitsu-122-w-58h.png',
+  },
+];
