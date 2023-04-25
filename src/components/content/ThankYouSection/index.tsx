861d5f3d703d56443299507fad5dc6bcd1948bbd
lilyx13 <ashlyn@lilyx.io>
2023-04-25 14:31:46 -0600

    horizontal scroll on mobile
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>

--- src/components/content/ThankYouSection/index.tsx
+++ src/components/content/ThankYouSection/index.tsx
@@ -10,24 +10,35 @@ function ThankYouSection(): JSX.Element {
           The Podman community has contributors from many different organizations, including:
         </p>
       </header>
-      <div className="container grid place-items-center lg:grid-cols-5">
-        <a href={redHat.href} target="_blank" className="row-span-2 row-start-1 block rounded-md p-4 dark:bg-gray-100">
-          <img {...redHat} />
-        </a>
+      <div className="relative mx-auto my-8">
+        <div
+          id="slider"
+          className="mx-auto h-full w-full place-items-center gap-6 overflow-x-scroll scroll-smooth whitespace-nowrap lg:container lg:grid lg:grid-cols-5">
+          <a
+            href={redHat.href}
+            target="_blank"
+            className="mx-4 mb-4 inline-block rounded-md p-4  dark:bg-gray-100 lg:row-span-2 lg:row-start-1 lg:mb-0">
+            <img {...redHat} className="mx-auto h-20 p-4 lg:h-32" />
+          </a>
 
-        {sponsors.map((logo, index) => {
-          return (
-            <a href={logo.href} key={index} target="_blank" className="block rounded-md p-4 dark:bg-gray-100">
-              <img {...logo} />
-            </a>
-          );
-        })}
-        <a
-          href={debian.href}
-          target="_blank"
-          className="col-start-5 row-span-2 row-start-1 block rounded-md p-4 dark:bg-gray-100 ">
-          <img {...debian} />
-        </a>
+          {sponsors.map((logo, index) => {
+            return (
+              <a
+                href={logo.href}
+                key={index}
+                target="_blank"
+                className="mx-4 mb-4 inline-block rounded-md p-4  dark:bg-gray-100 lg:mb-0">
+                <img {...logo} className="mx-auto h-20 p-4 lg:h-32" />
+              </a>
+            );
+          })}
+          <a
+            href={debian.href}
+            target="_blank"
+            className="mx-4 mb-4 inline-block rounded-md p-4  dark:bg-gray-100 lg:col-start-5  lg:row-span-2 lg:row-start-1 lg:mb-0">
+            <img {...debian} className="mx-auto h-20 p-4 lg:h-32" />
+          </a>
+        </div>
       </div>
     </section>
   );
