f930d3242dca7cd59afe145b507f5473e247f2d3
lilyx13 <ashlyn@lilyx.io>
2023-04-25 15:54:57 -0600

    grid flow columns improve layout
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>

--- src/components/content/ThankYouSection/index.tsx
+++ src/components/content/ThankYouSection/index.tsx
@@ -29,14 +29,13 @@ function ThankYouSection(): JSX.Element {
         </button>
         <div
           id="slider"
-          className="mx-auto h-full w-full place-items-center gap-6 overflow-x-scroll scroll-smooth whitespace-nowrap scrollbar scrollbar-track-purple-500 lg:container lg:grid lg:grid-cols-5">
+          className="mx-auto h-full w-full grid-flow-col place-items-center gap-6 overflow-x-scroll scroll-smooth whitespace-nowrap scrollbar scrollbar-track-purple-500 lg:container lg:grid lg:grid-rows-2">
           <a
             href={redHat.href}
             target="_blank"
             className="mx-4 mb-4 inline-block rounded-md p-4  dark:bg-gray-100 lg:row-span-2 lg:row-start-1 lg:mb-0">
             <img {...redHat} className="mx-auto h-20 p-4 lg:h-32" />
           </a>
-
           {sponsors.map((logo, index) => {
             return (
               <a
@@ -44,14 +43,14 @@ function ThankYouSection(): JSX.Element {
                 key={index}
                 target="_blank"
                 className="mx-4 mb-4 inline-block rounded-md p-4  dark:bg-gray-100 lg:mb-0">
-                <img {...logo} className="mx-auto h-20 p-4 lg:h-32" />
+                <img {...logo} className="mx-auto h-20 max-w-xs p-4 lg:h-32" />
               </a>
             );
           })}
           <a
             href={debian.href}
             target="_blank"
-            className="mx-4 mb-4 inline-block rounded-md p-4  dark:bg-gray-100 lg:col-start-5  lg:row-span-2 lg:row-start-1 lg:mb-0">
+            className="mx-4 mb-4 inline-block rounded-md p-4  dark:bg-gray-100  lg:row-span-2 lg:row-start-1 lg:mb-0">
             <img {...debian} className="mx-auto h-20 p-4 lg:h-32" />
           </a>
         </div>
