2b31afb288bfe4d3004ccfa87e9b683c4c98eca7
lilyx13 <ashlyn@lilyx.io>
2023-04-26 19:56:50 -0600

    search pull and list content
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>

--- src/components/content/CodeExampleSection/index.tsx
+++ src/components/content/CodeExampleSection/index.tsx
@@ -8,21 +8,22 @@ interface CodeProps {
   code: string;
   label: string;
   extra?: string;
+  className?: string;
 }
 const RenderLabelledCode = (props: CodeProps) => {
-  const { command, code, label, extra } = props;
+  const { command, code, label, extra, className } = props;
 
   return (
-    <div className="container">
-      <div className="mx-4 mb-2 mt-4 rounded-md bg-blue-700 p-4 text-center shadow-md dark:bg-blue-900 md:max-w-sm lg:mx-0 lg:text-start">
+    <div className={`w-96 md:max-w-md ${className}`}>
+      <div className="m-4 max-w-md rounded-md bg-blue-700 p-4 text-center shadow-md dark:bg-blue-900 md:max-w-sm md:text-start lg:mx-0">
         <Markdown text={label} styles="text-white dark:text-white" />
       </div>
       {extra && (
-        <div className="mx-4 mb-2 mt-4 hidden rounded-md bg-blue-700 p-4 shadow-md dark:bg-blue-900 md:block md:max-w-sm lg:mx-0">
+        <div className="mx-4 mb-2 mt-4 hidden max-w-md rounded-md bg-blue-700 p-4 shadow-md dark:bg-blue-900 md:mx-0 md:block md:max-w-sm">
           <Markdown text={extra} styles="text-white dark:text-white" />
         </div>
       )}
-      <CodeBlock language="bash" showLineNumbers className="mx-4 my-2 md:hidden">
+      <CodeBlock language="bash" showLineNumbers className="mx-4 my-2 max-w-md md:hidden">
         {command}
         {code}
       </CodeBlock>
@@ -30,14 +31,38 @@ const RenderLabelledCode = (props: CodeProps) => {
   );
 };
 
-const DesktopCodeBlock = () => {};
+const DesktopCodeBlock = ({ className }: string) => {
+  console.log(className);
+  return (
+    <div className={`container hidden md:block ${className}`}>
+      <CodeBlock language="bash" showLineNumbers>
+        {searchExample.command}
+        {searchExample.code}
+        {searchFilterExample.command}
+        {searchFilterExample.code}
+        {imagesExample.command}
+        {imagesExample.code}
+      </CodeBlock>
+    </div>
+  );
+};
 
 function CodeExampleSection() {
   return (
-    <div>
-      <RenderLabelledCode {...searchExample} />
-      <RenderLabelledCode {...searchFilterExample} />
-      <RenderLabelledCode {...imagesExample} />
+    <div className="container my-12 grid place-items-center gap-4 md:grid-cols-12 md:place-items-end">
+      <DesktopCodeBlock className="-z-50 md:col-span-11 md:col-start-1 md:row-start-1 md:row-end-4 md:place-self-center lg:col-span-10 lg:col-start-1" />
+      <RenderLabelledCode
+        {...searchExample}
+        className="row-start-1 md:col-span-5 md:col-start-9 md:self-end lg:col-span-3 lg:col-start-10"
+      />
+      <RenderLabelledCode
+        {...searchFilterExample}
+        className="row-start-2 md:col-span-5 md:col-start-9 lg:col-span-3 lg:col-start-10"
+      />
+      <RenderLabelledCode
+        {...imagesExample}
+        className="row-start-3 md:col-span-5 md:col-start-9 lg:col-span-3 lg:col-start-10"
+      />
     </div>
   );
 }
