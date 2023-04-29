c38b3c46a4801bedcb062ef497ae9aa4406248d1
lilyx13 <ashlyn@lilyx.io>
2023-04-29 15:23:00 -0600

    remove rehypeRaw to test if this fixes build
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>

--- src/components/utilities/Markdown/index.tsx
+++ src/components/utilities/Markdown/index.tsx
@@ -1,7 +1,7 @@
 import React, { lazy, Suspense } from 'react';
 import BrowserOnly from '@docusaurus/BrowserOnly';
-import { lazily } from 'react-lazily';
-const rehypeRaw = lazily(() => import('rehype-raw'));
+// import { lazily } from 'react-lazily';
+// const rehypeRaw = lazily(() => import('rehype-raw'));
 const ReactMarkdown = lazy(() => import('react-markdown'));
 interface Props {
   text: string;
@@ -11,13 +11,13 @@ interface Props {
 const fallBackComponent = () => {
   return <p>text loading...</p>;
 };
-
+// rehypePlugins={[rehypeRaw] as any}
 function Markdown({ text, styles }: Props): JSX.Element {
   return (
     <BrowserOnly>
       {() => (
         <Suspense fallback={fallBackComponent()}>
-          <ReactMarkdown children={text} className={styles} rehypePlugins={[rehypeRaw] as any} />
+          <ReactMarkdown children={text} className={styles} />
         </Suspense>
       )}
     </BrowserOnly>
