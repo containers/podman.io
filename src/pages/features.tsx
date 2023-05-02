28d78c2beb7e2c7ecbf7d673c5a5173d82093a3b
lilyx13 <ashlyn@lilyx.io>
2023-05-02 13:22:30 -0600

    shortened list of blog posts on features for better layout
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>

--- src/pages/features.tsx
+++ src/pages/features.tsx
@@ -35,18 +35,23 @@ function GetToKnowPodmanSection() {
 
 const PodmanDesktopSection = () => {
   return (
-    <section className="bg-gradient-to-b from-gray-50 to-gray-100 dark:bg-gray-900 dark:from-gray-700/25 dark:to-gray-900 pb-5">
-      <div className="container flex justify-center align-center">
+    <section className="bg-gradient-to-b from-gray-50 to-gray-100 pb-5 dark:bg-gray-900 dark:from-gray-700/25 dark:to-gray-900">
+      <div className="align-center container flex justify-center">
         <div className="flex-row content-center">
-         <h2 className="text-white dark:text-white bg-blue-500 dark:bg-blue-700 pl-5 pr-5 mb-5 content-center">Podman Desktop</h2>
+          <h2 className="mb-5 content-center bg-blue-500 pl-5 pr-5 text-white dark:bg-blue-700 dark:text-white">
+            Podman Desktop
+          </h2>
         </div>
       </div>
-      <div className="container grid gap-2 lg:grid-cols-3 mb-4">
+      <div className="container mb-4 grid gap-2 lg:grid-cols-3">
         <div id="imgdiv" className="mx-auto">
-          <img id="pdlogo" src="logos/optimized/podman-desktop-logo-200w-198h.webp"/>
+          <img id="pdlogo" src="logos/optimized/podman-desktop-logo-200w-198h.webp" />
         </div>
         <div className="col-span-2">
-          <p className="text-2xl leading-relaxed align-middle my-8">Podman Desktop is Podman's graphical application that makes it easy to install and work with Podman (and other container engines) on Windows, MacOS, and Linux.</p>
+          <p className="my-8 align-middle text-2xl leading-relaxed">
+            Podman Desktop is Podman's graphical application that makes it easy to install and work with Podman (and
+            other container engines) on Windows, MacOS, and Linux.
+          </p>
         </div>
       </div>
     </section>
@@ -55,20 +60,28 @@ const PodmanDesktopSection = () => {
 
 const ManageContainersUISection = () => {
   return (
-    <section className="bg-gradient-to-b from-purple-100 to-purple-300 dark:from-purple-900 dark:to-blue-900 pb-5">
-      <div className="container grid gap-4 md:grid-cols-3 justify-center align-center">
-        <div className="my-8 rounded-md bg-white/50 shadow-md h-fit">
+    <section className="bg-gradient-to-b from-purple-100 to-purple-300 pb-5 dark:from-purple-900 dark:to-blue-900">
+      <div className="align-center container grid justify-center gap-4 md:grid-cols-3">
+        <div className="my-8 h-fit rounded-md bg-white/50 shadow-md">
           <div className="flex flex-col items-center justify-center p-8 dark:text-black">
             <div>
               <h3 className="mb-5 dark:text-black">Manage containers (not just Podman.)</h3>
-              <p className="mb-3 dark:text-black">Podman Desktop allows you to list, view, and manage containers from multiple supported container engines* in a single unified view.</p>
-              <p className="mb-3 dark:text-black">Gain easy access to a shell inside the container, logs, and basic controls.</p>
-              <em className="block mt-10">* Supported engines and orchestrators include Podman, Docker, Lima, kind, Red Hat OpenShift, Red Hat OpenShift Developer Sandbox.</em>
+              <p className="mb-3 dark:text-black">
+                Podman Desktop allows you to list, view, and manage containers from multiple supported container
+                engines* in a single unified view.
+              </p>
+              <p className="mb-3 dark:text-black">
+                Gain easy access to a shell inside the container, logs, and basic controls.
+              </p>
+              <em className="mt-10 block">
+                * Supported engines and orchestrators include Podman, Docker, Lima, kind, Red Hat OpenShift, Red Hat
+                OpenShift Developer Sandbox.
+              </em>
             </div>
           </div>
         </div>
         <div className="col-span-2 w-full">
-            <img className="w-full" src="images/optimized/ui-screens/ui-manage-containers.webp" />
+          <img className="w-full" src="images/optimized/ui-screens/ui-manage-containers.webp" />
         </div>
       </div>
     </section>
@@ -77,58 +90,70 @@ const ManageContainersUISection = () => {
 
 const BuildImagesUISection = () => {
   return (
-    <section className="bg-gradient-to-b from-white to-gray-50 dark:from-gray-900 dark:to-purple-900 pb-5">
-    <div className="container grid gap-4 md:grid-cols-3 justify-center align-center">
-      <div className="col-span-2 w-full">
-            <img className="w-full md:max-w-[75%] mt-6 mb-6" src="images/optimized/ui-screens/ui-buildimage.webp" />
-      </div>
-      <div className="my-8 rounded-md bg-white/50 shadow-md h-fit">
-        <div className="flex flex-col items-center justify-center p-8">
-          <div>
-            <h3 className="mb-5 dark:text-black">Build, pull, and push images.</h3>
-            <p className="dark:text-black">Build containers from a Dockerfile / Containerfile, or pull images from remote repositories to run.</p>
-            <p className="dark:text-black">Manage accounts for and push your images to multiple container registries.</p>
+    <section className="bg-gradient-to-b from-white to-gray-50 pb-5 dark:from-gray-900 dark:to-purple-900">
+      <div className="align-center container grid justify-center gap-4 md:grid-cols-3">
+        <div className="col-span-2 w-full">
+          <img className="mb-6 mt-6 w-full md:max-w-[75%]" src="images/optimized/ui-screens/ui-buildimage.webp" />
+        </div>
+        <div className="my-8 h-fit rounded-md bg-white/50 shadow-md">
+          <div className="flex flex-col items-center justify-center p-8">
+            <div>
+              <h3 className="mb-5 dark:text-black">Build, pull, and push images.</h3>
+              <p className="dark:text-black">
+                Build containers from a Dockerfile / Containerfile, or pull images from remote repositories to run.
+              </p>
+              <p className="dark:text-black">
+                Manage accounts for and push your images to multiple container registries.
+              </p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  </section>
+    </section>
   );
 };
 
 const CreatePodsUISection = () => {
   return (
-    <section className="bg-gradient-to-b from-blue-100 to-blue-50 dark:from-gray-700 dark:to-gray-900 pb-5">
-      <div className="container grid gap-4 md:grid-cols-3 justify-center align-right">
-        <div className="my-8 rounded-md bg-white/50 shadow-md h-fit">
+    <section className="bg-gradient-to-b from-blue-100 to-blue-50 pb-5 dark:from-gray-700 dark:to-gray-900">
+      <div className="align-right container grid justify-center gap-4 md:grid-cols-3">
+        <div className="my-8 h-fit rounded-md bg-white/50 shadow-md">
           <div className="flex flex-col items-center justify-center p-8">
-        <div>
-          <h3 className="mb-5 dark:text-black">Create pods.</h3>
-          <p className="dark:text-black">Create pods by selecting containers to run together. View unified logs for your pods and inspect the containers inside each.</p>
-          <p className="dark:text-black">Play Kubernetes YAML locally, without Kubernetes, and generate Kubernetes YAML from Pods.</p>
+            <div>
+              <h3 className="mb-5 dark:text-black">Create pods.</h3>
+              <p className="dark:text-black">
+                Create pods by selecting containers to run together. View unified logs for your pods and inspect the
+                containers inside each.
+              </p>
+              <p className="dark:text-black">
+                Play Kubernetes YAML locally, without Kubernetes, and generate Kubernetes YAML from Pods.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="col-span-2 w-full">
+          <img className="mb-6 mt-6 w-full md:max-w-[75%]" src="images/optimized/ui-screens/ui-podify.webp" />
+        </div>
       </div>
-    </div>
-  </div>
-  <div className="col-span-2 w-full">
-      <img className="w-full md:max-w-[75%] mt-6 mb-6" src="images/optimized/ui-screens/ui-podify.webp" />
-  </div>
-</div>
-</section>
+    </section>
   );
 };
 
 const DeployToKubernetesUISection = () => {
   return (
-    <section className="bg-gradient-to-b from-white to-gray-100 dark:from-blue-900 dark:to-gray-900 pb-5">
-      <div className="container grid gap-4 md:grid-cols-3 justify-center align-center">
+    <section className="bg-gradient-to-b from-white to-gray-100 pb-5 dark:from-blue-900 dark:to-gray-900">
+      <div className="align-center container grid justify-center gap-4 md:grid-cols-3">
         <div className="col-span-2 w-full">
-          <img className="w-full md:max-w-[75%] mt-6 mb-6" src="images/optimized/ui-screens/ui-k8sdeploy.webp" />
+          <img className="mb-6 mt-6 w-full md:max-w-[75%]" src="images/optimized/ui-screens/ui-k8sdeploy.webp" />
         </div>
-        <div className="my-8 rounded-md bg-white/50 shadow-md h-fit">
+        <div className="my-8 h-fit rounded-md bg-white/50 shadow-md">
           <div className="flex flex-col items-center justify-center p-8">
             <div>
-             <h3 className="mb-5 dark:text-black">Deploy to Kubernetes.</h3>
-             <p className="dark:text-black">Deploy pods from Podman Desktop to local or remote Kubernetes contexts using automatically-generated YAML config.</p>
+              <h3 className="mb-5 dark:text-black">Deploy to Kubernetes.</h3>
+              <p className="dark:text-black">
+                Deploy pods from Podman Desktop to local or remote Kubernetes contexts using automatically-generated
+                YAML config.
+              </p>
             </div>
           </div>
         </div>
@@ -139,18 +164,20 @@ const DeployToKubernetesUISection = () => {
 
 const PodmanCLISection = () => {
   return (
-    <section className="bg-gradient-to-b from-gray-50 to-gray-100  dark:from-gray-700/25 dark:to-gray-900  pb-5">
-      <div className="container flex justify-center align-center">
+    <section className="bg-gradient-to-b from-gray-50 to-gray-100  pb-5 dark:from-gray-700/25  dark:to-gray-900">
+      <div className="align-center container flex justify-center">
         <div className="flex-row content-center">
-         <h2 className="text-white dark:text-white bg-blue-700 pl-5 pr-5 mb-5 content-center">Podman Command-Line</h2>
+          <h2 className="mb-5 content-center bg-blue-700 pl-5 pr-5 text-white dark:text-white">Podman Command-Line</h2>
         </div>
       </div>
-      <div className="container grid gap-2 lg:grid-cols-3 mb-4">
+      <div className="container mb-4 grid gap-2 lg:grid-cols-3">
         <div className="mx-auto">
-          <img className="max-h-[200px]" src="images/raw/podman-selkie-385w-358h.png"/>
+          <img className="max-h-[200px]" src="images/raw/podman-selkie-385w-358h.png" />
         </div>
         <div className="col-span-2">
-          <p className="text-2xl leading-relaxed align-middle my-8">Podman's command-line interface allows you to find, run, build, and share containers.</p>
+          <p className="my-8 align-middle text-2xl leading-relaxed">
+            Podman's command-line interface allows you to find, run, build, and share containers.
+          </p>
         </div>
       </div>
     </section>
@@ -175,22 +202,33 @@ const LearnArticles = () => {
       <header className="container mb-4 text-center lg:mb-8 lg:text-start">
         <h3 className="font-medium text-blue-700 dark:text-blue-500">{learnMore.blogPosts.title}</h3>
       </header>
-      <div>
+      <div className="flex flex-col gap-4">
         {blogData.map((card, index) => {
-          return (
-            <ArticleCard
-              title={card.title.rendered}
-              author_link={card.author_info.author_link}
-              display_name={card.author_info.display_name}
-              subtitle={card.excerpt.rendered}
-              date={card.wbDate}
-              imgSrc={card.jetpack_featured_media_url}
-              path={card.link}
-              altLayout
-              key={card.id}
-            />
-          );
+          if (index < 2) {
+            return (
+              <ArticleCard
+                title={card.title.rendered}
+                author_link={card.author_info.author_link}
+                display_name={card.author_info.display_name}
+                subtitle={card.excerpt.rendered}
+                date={card.wbDate}
+                imgSrc={card.jetpack_featured_media_url}
+                path={card.link}
+                altLayout
+                key={card.id}
+              />
+            );
+          }
         })}
+        <p className="ml-2l">
+          Check out more posts about Podman{' '}
+          <a
+            href="https://blog.podman.io"
+            target="_blank"
+            className="underline-offset-4 transition duration-150 ease-linear hover:text-purple-700 dark:hover:text-purple-500">
+            on our Blog!
+          </a>
+        </p>
       </div>
     </section>
   );
