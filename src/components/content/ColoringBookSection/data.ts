b279cb058523183dae952adf4690910105059eca
lilyx13 <ashlyn@lilyx.io>
2023-05-08 13:14:10 -0600

    added coloring book to download
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>

--- src/components/content/ColoringBookSection/data.ts
+++ src/components/content/ColoringBookSection/data.ts
@@ -4,7 +4,7 @@ export default {
     "A decentralized team of open source container tool superheroes comes to the rescue when an asteroid storm threatens the planet. Learn about each tool—Podman, CRI-O, Buildah, Skopeo, and OpenShift—as they redesign the planet's protective shields' container deployment to protect Earth.",
   button: {
     text: 'Download',
-    src: '#',
+    src: 'https://developers.redhat.com/e-books/container-commandos-coloring-book',
   },
   featureImage: {
     src: 'images/raw/comic-cover-224w-288h.png',
