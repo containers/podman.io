1d83e02d1a99de4b0b5c9763d3f394ff0bf90911
Máirín Duffy <duffy@redhat.com>
2023-05-12 00:02:39 -0400

    Updating phrasing on hero download button
    Making clear for now the payloads are Podman Desktop. Helps address
    issue #83. Will be fixed more fully in issue #116
    
    Signed-off-by: Máirin Duffy <duffy@redhat.com>

--- src/components/layout/HeroHeader/installOptions.ts
+++ src/components/layout/HeroHeader/installOptions.ts
@@ -2,8 +2,8 @@ import { LATEST_VERSION, LATEST_DESKTOP_VERSION } from '@site/static/data/global
 const operatingSystemData = [
   {
     id: 'windows',
-    title: 'Download for Windows',
-    subtitle: `Install on Desktop v-${LATEST_DESKTOP_VERSION}`,
+    title: 'Download Podman Desktop for Windows',
+    subtitle: `Windows Installer v-${LATEST_DESKTOP_VERSION}`,
     icon: 'fa-brands:windows',
     options: [],
     path: `https://github.com/containers/podman-desktop/releases/download/v${LATEST_DESKTOP_VERSION}/podman-desktop-${LATEST_DESKTOP_VERSION}-setup.exe`,
@@ -15,7 +15,7 @@ const operatingSystemData = [
   },
   {
     id: 'macintosh',
-    title: 'Download for macOS',
+    title: 'Download Podman Desktop for macOS',
     subtitle: `Universal *.dmg v-${LATEST_DESKTOP_VERSION}`,
     icon: 'fa-brands:apple',
     options: [],
@@ -23,13 +23,13 @@ const operatingSystemData = [
     other: {
       path: 'https://podman-desktop.io/downloads',
       text: 'Other Install Options',
-      subtext: '(Including macOS Intel & Arm builds and other Os options)',
+      subtext: '(Including macOS Intel & Arm builds and other OS options)',
     },
   },
   {
     id: 'linux',
-    title: 'Download for Linux',
-    subtitle: `Install Desktop v-${LATEST_DESKTOP_VERSION}`,
+    title: 'Download Podman Desktop for Linux',
+    subtitle: `Flatpak v-${LATEST_DESKTOP_VERSION}`,
     icon: 'fa-brands:linux',
     options: [{ path: 'https://podman.io/getting-started/installation#installing-on-linux', text: 'CLI Install' }],
     path: `https://github.com/containers/podman-desktop/releases/download/v${LATEST_DESKTOP_VERSION}/podman-desktop-${LATEST_DESKTOP_VERSION}.flatpak`,
