32ffa967dad17d1b8c893128baf9f65e8b40fbd5
Máirín Duffy <duffy@redhat.com>
2023-05-13 15:43:40 -0400

    Aesthetic tweaks to testimonial cards
    - fixing some responsive issues with avatars at small screen widths
    - adjusting testimonial body text leading
    - adding tweet link to date at bottom
    - changing alignment of date to always be on bottom of card
    - color tweaks for date at bottom including link+hover
    - color/appearance tweaks for username text at top
    
    Signed-off-by: Máirín Duffy <duffy@redhat.com>

--- src/components/ui/Testimonial/index.tsx
+++ src/components/ui/Testimonial/index.tsx
@@ -13,26 +13,28 @@ type TestimonialProps = {
 
 function Testimonial(props: TestimonialProps) {
   return (
-    <article className="mx-2 my-4 max-w-sm rounded-sm bg-white p-4 shadow-lg dark:bg-gray-900">
-      <div className="flex items-center gap-3">
-        <div className="mt-2">
+    <article className="flex flex-col mx-2 my-4 max-w-sm rounded-sm bg-white p-4 shadow-lg dark:bg-gray-900">
+      <div className="flex items-center gap-3 mb-4">
+        <div className="m-2">
           <div className="flex items-center gap-2">
             <h3 className="text-lg font-bold">{props.name}</h3>
             <Icon icon={`logos:${props.social}`} className="text-2xl" />
           </div>
-          <a href={props.path} className=" text-gray-700">
+          <a href={props.path} className=" text-gray-700 dark:text-gray-100 dark:hover:text-purple-900 no-underline hover:no-underline hover:bg-purple-300">
             {props.handle}
           </a>
         </div>
         <div className="order-first">
-          <img src={`${props.avatar}`} alt="user avatar" className="h-12 w-12 rounded-full" />
+          <img src={`${props.avatar}`} alt="user avatar" className="h-fit w-fit max-w-16 max-h-16 rounded-full" />
         </div>
       </div>
-      <div className="my-2">
-        <p className="whitespace-normal text-gray-900 dark:text-gray-300">{props.description}</p>
+      <div className="mt-2 mb-4">
+        <p className="whitespace-normal text-gray-900 dark:text-gray-300 leading-snug">{props.description}</p>
       </div>
-      <div className="self-start text-gray-500">
-        <p>{props.date}</p>
+      <div className="mt-auto self-start text-gray-300 dark:text-gray-700 italic">
+        <a href={props.path} className="text-gray-300 dark:text-gray-700 dark:hover:text-gray-700 no-underline hover:no-underline hover:bg-purple-300">
+          {props.date}
+        </a>
       </div>
     </article>
   );
