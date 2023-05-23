ec889dcd9d97e3bc8ba973d2f44400882140ba1c
Florent BENOIT <fbenoit@redhat.com>
2023-05-23 04:16:09 +0200

    feat: Podman Desktop v1.0.1 is the latest (#142)
    Change-Id: I14090f7124d87f79b2803c7f18b6c476e011b252
    
    
    Change-Id: I3ca95d37fcdea200c62e3b9e5cb95c9a17324e8b
    
    Signed-off-by: Florent BENOIT <fbenoit@redhat.com>

--- static/data/global.ts
+++ static/data/global.ts
@@ -1,4 +1,4 @@
 export const LATEST_VERSION = '4.5.0';
-export const LATEST_DESKTOP_VERSION = '1.0.0';
+export const LATEST_DESKTOP_VERSION = '1.0.1';
 export const MEETING_URL = 'https://meet.google.com/ieq-pxhy-jbh';
 export const RELEASE_NOTES = '';
