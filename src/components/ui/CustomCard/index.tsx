041059542db17819a4c83a8526833270347395ff
cgiradkar <cgiradka@redhat.com>
2023-06-09 13:01:56 +0100

    Add visual components with transition

--- src/components/ui/CustomCard/index.tsx
+++ src/components/ui/CustomCard/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Button from '@site/src/components/utilities/Button/';
 import Markdown from '@site/src/components/utilities/Markdown';
+import FilmIcon from '../../shapes/FilmIcon';
 
 function CardHeader(props) {
   const { title, subtitle, details } = props;
   return (
-    <div className="mx-2 mt-4">
+    <div className="mx-2 mb-10 mt-4 text-center">
       <h3 className="mb-3 font-bold text-gray-700 dark:text-gray-50">{title}</h3>
       <Markdown text={subtitle} styles="text-gray-700" />
       <Markdown text={details} styles="text-gray-700" />
@@ -26,7 +27,7 @@ function CardBody(props) {
 // There is a 'CardButtons' mode which can be attained by passing parimary flag as true
 function CardInfoButtons({ data = [{ text: 'button text' }], primary = false }) {
   return (
-    <div className="align-center mb-4 flex flex-row flex-wrap justify-center gap-4 lg:mb-8 2xl:px-10">
+    <div className="align-center mb-4 mt-8 flex flex-row flex-wrap justify-center gap-4 lg:mb-8 2xl:px-10">
       {primary
         ? data.map((button, index) => (
             <div key={index}>
@@ -45,10 +46,10 @@ function CardInfoButtons({ data = [{ text: 'button text' }], primary = false }) 
 function CustomCard(props) {
   return (
     <article
-      style={props.primary ? { height: '500px' } : {}}
+      style={props.primary ? { height: '500px', flex: 1 } : {}}
       className="flex w-11/12 flex-col rounded-lg bg-gray-50 p-4 shadow-xl dark:bg-gray-700 dark:shadow-none lg:mx-8 lg:my-4">
       <CardHeader {...props} />
-      <CardBody {...props} />
+      {props?.icon ? <FilmIcon /> : <CardBody {...props} />}
       <CardInfoButtons {...props} />
     </article>
   );
