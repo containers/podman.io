41e1e82f9870140c3681e86cf2a6c365fde4004c
Denis Golovin <dgolovin@redhat.com>
2023-08-17 23:36:38 -0700

    chore: update Podman Desktop version to v1.3.1
    Signed-off-by: Denis Golovin <dgolovin@redhat.com>

--- static/data/global.ts
+++ static/data/global.ts
@@ -1,5 +1,5 @@
 export const LATEST_VERSION = '4.6.1';
-export const LATEST_DESKTOP_VERSION = '1.2.1';
-export const LATEST_DESKTOP_DOWNLOAD_URL = 'https://podman-desktop.io/blog/podman-desktop-release-1.2';
+export const LATEST_DESKTOP_VERSION = '1.3.1';
+export const LATEST_DESKTOP_DOWNLOAD_URL = 'https://podman-desktop.io/blog/podman-desktop-release-1.3';
 export const MEETING_URL = 'https://meet.google.com/ieq-pxhy-jbh';
 export const RELEASE_NOTES = '';
