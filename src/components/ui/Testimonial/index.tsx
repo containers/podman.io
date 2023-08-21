1180fadaba5a86085ff3ecd2dc41eea999bcd08f
Daniel Sage <24928223+dnsge@users.noreply.github.com>
2023-08-21 12:55:38 -0400

    Update src/components/ui/Testimonial/index.tsx
    Co-authored-by: cgiradkar <135707592+cgiradkar@users.noreply.github.com>
    Signed-off-by: Daniel Sage <sagedanielr@gmail.com>

--- src/components/ui/Testimonial/index.tsx
+++ src/components/ui/Testimonial/index.tsx
@@ -31,7 +31,7 @@ function Testimonial(props: TestimonialProps) {
       </div>
       <div className="mt-2 mb-4 truncate">
         <p className="whitespace-normal text-gray-900 dark:text-gray-300 leading-snug mb-2">{props.description}</p>
-        {props.featuredlink && <a href={props.featuredlink}>{props.featuredlink}</a>}
+        {props.featuredlink && <a target="_blank" href={props.featuredlink}>{props.featuredlink}</a>}
       </div>
       <div className="mt-auto self-start text-gray-300 dark:text-gray-700 italic">
         <a href={props.path} className="text-gray-300 dark:text-gray-700 dark:hover:text-gray-700 no-underline hover:no-underline hover:bg-purple-300">
