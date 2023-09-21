75cf881044afec485ae13be77dda655255b68925
TomSweeneyRedHat <tsweeney@redhat.com>
2023-09-21 16:23:24 -0400

    Touch up meeting room and desktop post
    Update the community meeting room location and the latest
    Podman Desktop version (1.4) blog post.
    
    Signed-off-by: TomSweeneyRedHat <tsweeney@redhat.com>

--- static/data/global.ts
+++ static/data/global.ts
@@ -1,5 +1,5 @@
 export const LATEST_VERSION = '4.6.2';
 export const LATEST_DESKTOP_VERSION = '1.4.0';
-export const LATEST_DESKTOP_DOWNLOAD_URL = 'https://podman-desktop.io/blog/podman-desktop-release-1.3';
-export const MEETING_URL = 'https://meet.google.com/ieq-pxhy-jbh';
+export const LATEST_DESKTOP_DOWNLOAD_URL = 'https://podman-desktop.io/blog/podman-desktop-release-1.4';
+export const MEETING_URL = 'https://meet.google.com/xrq-uemd-bzy';
 export const RELEASE_NOTES = '';
