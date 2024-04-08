6b078ed7b6e07c06da48bc59564aea1196253ff7
benoitf <fbenoit@redhat.com>
2024-04-08 11:29:39 +0000

    chore: Update Podman Desktop version to v1.9.1
    Signed-off-by: benoitf <fbenoit@redhat.com>

--- static/data/global.ts
+++ static/data/global.ts
@@ -1,5 +1,5 @@
 export const LATEST_VERSION = '5.0.1';
-export const LATEST_DESKTOP_VERSION = '1.8.0';
-export const LATEST_DESKTOP_DOWNLOAD_URL = 'https://podman-desktop.io/blog/podman-desktop-release-1.8';
+export const LATEST_DESKTOP_VERSION = '1.9.1';
+export const LATEST_DESKTOP_DOWNLOAD_URL = 'https://podman-desktop.io/blog/podman-desktop-release-1.9';
 export const MEETING_URL = 'https://meet.google.com/xrq-uemd-bzy';
 export const RELEASE_NOTES = '';
