4105c985c1029a1e087526eeed534c3482b8e5b3
mheon <7735048+mheon@users.noreply.github.com>
2024-10-23 17:49:07 +0000

    Bump Podman to v5.2.5
    Signed-off-by: mheon <7735048+mheon@users.noreply.github.com>

--- static/data/global.ts
+++ static/data/global.ts
@@ -1,4 +1,4 @@
-export const LATEST_VERSION = '5.2.4';
+export const LATEST_VERSION = '5.2.5';
 export const LATEST_DESKTOP_VERSION = '1.13.2';
 export const LATEST_DESKTOP_DOWNLOAD_URL = 'https://podman-desktop.io/blog/podman-desktop-release-1.13';
 export const MEETING_URL = 'https://meet.google.com/xrq-uemd-bzy';
