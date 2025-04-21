51f5aaeb1709f5b965712320f2661d2fb4d5b90a
SoniaSandler <fbenoit@redhat.com>
2025-04-21 14:30:08 +0000

    chore: Update Podman Desktop version to v1.18.0
    Signed-off-by: SoniaSandler <fbenoit@redhat.com>

--- static/data/global.ts
+++ static/data/global.ts
@@ -1,5 +1,5 @@
 export const LATEST_VERSION = '5.4.2';
-export const LATEST_DESKTOP_VERSION = '1.17.2';
-export const LATEST_DESKTOP_DOWNLOAD_URL = 'https://podman-desktop.io/blog/podman-desktop-release-1.17';
+export const LATEST_DESKTOP_VERSION = '1.18.0';
+export const LATEST_DESKTOP_DOWNLOAD_URL = 'https://podman-desktop.io/blog/podman-desktop-release-1.18';
 export const MEETING_URL = 'https://meet.google.com/xrq-uemd-bzy';
 export const RELEASE_NOTES = '';
