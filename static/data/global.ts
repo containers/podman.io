c57f96dcf92b8aafbecfaa68cb4d3742533e4b9a
feloy <fbenoit@redhat.com>
2025-08-25 13:44:52 +0000

    chore: Update Podman Desktop version to v1.21.0
    Signed-off-by: feloy <fbenoit@redhat.com>

--- static/data/global.ts
+++ static/data/global.ts
@@ -1,5 +1,5 @@
 export const LATEST_VERSION = '5.6.0';
-export const LATEST_DESKTOP_VERSION = '1.20.2';
-export const LATEST_DESKTOP_DOWNLOAD_URL = 'https://podman-desktop.io/blog/podman-desktop-release-1.20';
+export const LATEST_DESKTOP_VERSION = '1.21.0';
+export const LATEST_DESKTOP_DOWNLOAD_URL = 'https://podman-desktop.io/blog/podman-desktop-release-1.21';
 export const MEETING_URL = 'https://meet.google.com/xrq-uemd-bzy';
 export const RELEASE_NOTES = '';
