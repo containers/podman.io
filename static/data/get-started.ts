696179acbada509e227dd70ac61f8f65e84648d4
sanketkokate712 <sanketkokate712@gmail.com>
2026-08-18 04:58:29 +0530

    Fix #48: Add Podman Desktop steps to Get Started

--- static/data/get-started.ts
+++ static/data/get-started.ts
@@ -5,11 +5,18 @@ const header = {
   instructions: {
     title: 'First Things First: Installing Podman',
     subtitle: 'For installing or building Podman, please see the installation instructions:',
-    button: {
-      text: 'Installation Instructions',
-      path: 'docs/installation',
-      icon: 'fa6-solid:book',
-    },
+    buttons: [
+      {
+        text: 'Installation Instructions',
+        path: 'docs/installation',
+        icon: 'fa6-solid:book',
+      },
+      {
+        text: 'Podman Desktop',
+        path: 'https://podman-desktop.io/downloads',
+        icon: 'fa6-solid:desktop',
+      },
+    ],
   },
 };
 
